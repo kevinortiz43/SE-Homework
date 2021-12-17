5abb004aa399ad2e3c3b125b70b8d35e512b42f8
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2021-12-17 14:59:55 -0500

    updated complaints_app HW

--- complaints_app/app.js
+++ complaints_app/app.js
@@ -10,43 +10,35 @@ const url2 = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD&
 const url3 = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD&borough=QUEENS`;
 const url4 = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD&borough=STATEN ISLAND`;
 const url5 = `https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD&borough=BRONX`;
-const counter = 0;
+
 
 displayPoliceResolution = (resolutionDescription, elementId) =>
 { let pTagSelector = document.querySelector(`#description${elementId}`)
     if( pTagSelector !=null && pTagSelector.style.display != "none"){
-        pTagSelector.style.display = "none"
-       
+        pTagSelector.style.display = "none"       
     }
-    else if(pTagSelector != null){ 
-        pTagSelector.style.display = "block"
+  
+    else if(pTagSelector != null){ pTagSelector.style.display = "block"}
     
-    }else document.getElementById(elementId).innerHTML += `<p id ="description${elementId}"> ${resolutionDescription} </p>`
-} ;
+    else document.getElementById(elementId).innerHTML += `<p id ="description${elementId}"> ${resolutionDescription} </p>`};
 
   fetchingTheFetch = (Boroughs) => {
         let userInput2 = userInput.value;
         fetch(Boroughs)
             .then((res) => res.json())
             .then((data) => {
-                
-            console.log(data);
             displayDescriptor.innerHTML = `<ul>`;
             for (let i = 1; i <= userInput2; i++)
             displayDescriptor.innerHTML += `<li id = "row${i}" > ${data[i].descriptor} 
             <button onclick="displayPoliceResolution('${data[i].resolution_description}','row${i}')" >   what did the police do? </button></li>`;
             displayDescriptor.innerHTML += `</ul>`})
-            .catch((error) => {  console.log(error)});
+            .catch((error) => {console.log(error)});
         };
 
         brooklynButton.addEventListener("click", () => {fetchingTheFetch(url1)});
-        
         manhattanButton.addEventListener("click", () => {fetchingTheFetch(url2)});
-
         queensButton.addEventListener("click", () => { fetchingTheFetch(url3)});
-        
         statenIslandButton.addEventListener("click", () => {fetchingTheFetch(url4)});
-        
         theBronxButton.addEventListener("click", () => { fetchingTheFetch(url5)});
 
 
