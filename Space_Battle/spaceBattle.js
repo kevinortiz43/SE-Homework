46b70ec4bf3ca57eaf8398ecf86b75685a41998e
kevinortiz43 <93737536+kevinortiz43@users.noreply.github.com>
2021-12-22 21:11:17 -0500

    Space Battle HW

--- Space_Battle/spaceBattle.js
+++ Space_Battle/spaceBattle.js
@@ -1,13 +1,11 @@
 let attackButton = document.getElementById("attackButton");
-let displayUserHull = document.getElementById("hull");
-let displayUserFirePower = document.getElementById("firepower");
-let displayUserAccuracy = document.getElementById("accuracy");
-let displayEnemyHull = document.getElementById("EHull");
-let displayEnemyFirePower = document.getElementById("EFirePower");
-let displayEnemyAccuracy = document.getElementById("EAccuracy");
-let displayMessage1 = document.getElementById("messages")
-
-
+let displayUserHull = document.getElementById("user-Hull");
+let displayUserFirePower = document.getElementById("user-FirePower");
+let displayUserAccuracy = document.getElementById("user-Accuracy");
+let displayEnemyHull = document.getElementById("enemy-Hull");
+let displayEnemyFirePower = document.getElementById("enemy-FirePower");
+let displayEnemyAccuracy = document.getElementById("enemy-Accuracy");
+let displayMessage1 = document.getElementById("messages");
 
 let enemyCount = 7;
 
@@ -15,38 +13,38 @@ const user = {
   hull: 20,
   firePower: 5,
   accuracy: 0.7,
-  
+
   attack(alien) {
-      alert(` User turn `);
-      
+    alert(` User turn `);
+
     let toHit = Math.random();
-    let userInput1 = prompt(`Do you want to attack or retreat?`);
+    let userInput1 = prompt(`Do you want to attack or retreat? `);
     if (this.hull > 0) {
       if (enemyCount > 0) {
         if (userInput1.toLowerCase() == `attack`) {
           if (this.accuracy > toHit) {
             alien.hull = alien.hull - this.firePower;
             if (alien.hull > 0) {
-              alert (` Enemy has ${alien.hull} hull point(s) left. `);
+              alert(` Enemy has ${alien.hull} hull point(s) left. `);
             } else {
-              alert (` You have destroyed the enemy! `);
-              alert (` There are ${enemyCount} ships left `);
+              alert(` You have destroyed the enemy! `);
               enemyCount--;
+              alert(` There are ${enemyCount} ships left `);
             }
           } else {
-            alert (` You have missed the alien. `);
+            alert(` You have missed the alien. `);
             enemy.attack1;
           }
         } else if (userInput1.toLowerCase() == `retreat`) {
-          alert (` You were routed, Game over `);
+          alert(` You were routed, Game over `);
         } else {
-          alert (` Please input appropriate response `);
+          alert(` Please input appropriate response `);
         }
       } else {
-        alert (` You destroyed all enemy ships! `);
+        alert(` You destroyed all enemy ships! `);
       }
     } else {
-      alert (` you were killed`);
+      alert(` you were killed`);
     }
     return alert;
   },
@@ -56,25 +54,24 @@ const enemy = {
   hull: Math.round(Math.random() * (6 - 3) + 3), //randomize hull strength //
   firePower: Math.round(Math.random() * (4 - 2) + 2), //randomize firepower //2-4
   accuracy: Math.random() * (0.8 - 0.6) + 0.6, //*  // randomize accuracy
-  
+
   attack1(human) {
     alert(` Enemy turn `);
-    
+
     let toHit = Math.random();
     if (human.hull > 0) {
       if (this.accuracy > toHit) {
         human.hull = human.hull - this.firePower;
         if (human.hull > 0) {
           alert(` You were hit! you have ${human.hull} hull points left `);
-          
         } else {
           alert(` You were killed `);
         }
       } else {
         alert(` The enemy missed `);
-            }
-        } else {
-     }
+      }
+    } else {
+    }
     return alert;
   },
 };
@@ -96,21 +93,18 @@ function displayEnemyStats() {
 }
 
 function displayMessage(x) {
-    alert(x);
+  alert(x);
+}
+
+function displayBattle() {
+  if (user.hull > 0 && enemy.hull > 0) {
+    while (enemyCount > -1 && user.hull > 0) {
+      console.log(user.attack(enemy));
+      console.log(enemy.attack1(user));
+    }
   }
-
-
-
-function displayBattle (){
-    if (user.hull > 0 && enemy.hull > 0) {
-        while (enemyCount > -1 && user.hull > 0 ) { //review
-          console.log(user.attack(enemy));
-          console.log(enemy.attack1(user));
-        } 
-      }  
 }
 
 attackButton.addEventListener("click", () => {
-  console.log(displayBattle())
-    }
-);
+  console.log(displayBattle());
+});
